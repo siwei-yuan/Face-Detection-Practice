d9bb3b63114fe6b08ffbd696e4f1212dc0a19215
siwei-yuan <siwei_yuan@hotmail.com>
2021-10-03 15:58:41 -0700

    file path change

--- main/readin_pic.py
+++ main/readin_pic.py
@@ -3,7 +3,7 @@ import cv2 as cv
 
 def face_detect_func():
     gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    face_detect = cv.CascadeClassifier('C:/Users/YSW/Desktop/Face_Detection/venv/Lib/site-packages/cv2/data'
+    face_detect = cv.CascadeClassifier('C:/Users/YSW/Desktop/Face-Detection-Practice/venv/Lib/site-packages/cv2/data'
                                        '/haarcascade_frontalface_alt2.xml')
     face = face_detect.detectMultiScale(gray_img, 1.01, 5, 0, (100, 100), (300, 300))
     for x, y, w, h in face:
